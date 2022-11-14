df0a5e43f1dbf1bb8cac193b4ddb8f3d384b4280
yeren <xchosenc@gmail.com>
2022-11-14 14:20:23 -0500

    init

---
+++ web/src/component/FunctionBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Grid from '@mui/material/Grid';
+import Avatar from './Avatar';
+
+export default () => {
+    return (
+        <Grid container direction='column'>
+            <Grid item xs={2}>
+                <Avatar>sdsd</Avatar>
+            </Grid>
+            <Grid item xs={4}>
+            </Grid>
+        </Grid>
+    )
+}
