a0320daaf48f782c53181a94c2a5f222737ef810
dzwfighting <yiwen7943@gmail.com>
2022-12-01 18:50:32 -0500

    complete add friend and delete friend

---
+++ server/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+require("express-async-errors");
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const store = require("./store/dataStore");
+const configRoutes = require("./routes/router");
+
+const cookieParser = require("cookie-parser");
+
+const usersCollection = require("./data/users");
+
+app.use(cookieParser());
+
+app.use(express.json());
+
+app.use(async (req, res, next) => {
+  console.log("Url ", req.originalUrl, " is in...");
+  if (
+    req.originalUrl === "/login" ||
+    (req.originalUrl === "/user" && req.method == "POST")
+  )
+    next();
+  else {
+    let session = req.cookies[store.SESSION_KEY];
+    if (!session) throw "Please login in.";
+    let user = await usersCollection.getUser(session);
+    console.log("\tCurrent user is ", user.name);
+    if (!user) throw "Please login in.";
+    console.log("\tAuth verified.");
+    next();
+  }
+});
+
+configRoutes(app);
+
+app.use((err, req, res, next) => {
+  // logic
+  if (err.stack) {
+    console.error(err.stack);
+    res.status(400).send({ errMsg: err.stack });
+  } else {
+    res.status(400).send({ errMsg: err });
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(4000, () => {
+  console.log("listening on *:http://localhost:4000");
+});
---
+++ server/response/response.js
@@ -0,0 +1,44 @@
+// const JSON = require('JSON')
+CODE_ERROR = 400;
+CODE_SUCCESS = 200;
+
+class Result {
+  constructor(data, msg = "success") {
+    if (data) {
+      this.data = data;
+    }
+    this.msg = msg;
+  }
+
+  createResult() {
+    let base = {
+      code: this.code,
+      msg: this.msg,
+    };
+    if (this.data) {
+      base.data = this.data;
+    }
+
+    return base;
+  }
+
+  success(res, code = CODE_SUCCESS, resCode = 200) {
+    if (!res) {
+      throw "Response is invalid.";
+    }
+    res.status(resCode);
+    this.code = code;
+    return this.createResult();
+  }
+
+  fail(res, code = CODE_ERROR, resCode = 400) {
+    if (!res) {
+      throw "Response is invalid.";
+    }
+    res.status(resCode);
+    this.code = code;
+    return this.createResult();
+  }
+}
+
+module.exports = Result;
