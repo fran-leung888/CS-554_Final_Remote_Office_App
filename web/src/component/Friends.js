1be39496692b187c2a0e3434079605140aa26fc6
dzwfighting <yiwen7943@gmail.com>
2022-12-11 19:46:49 -0500

    finish show friends list part

--- web/src/component/Friends.js
+++ web/src/component/Friends.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import ListGroup from "react-bootstrap/ListGroup";
 import Button from "react-bootstrap/Button";
+
+import users from "../data/users";
 
 export default function Friends() {
   const [hasFriend, setHasFriend] = useState(false);
   const [friendsData, setFriendsData] = useState(undefined);
+  //   const [deleteSuccess, setDeleteSuccess] = useState(false);
   const curUser = useSelector((state) => state.user);
   console.log(`curUser: ${JSON.stringify(curUser)}`);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (curUser.friends.length) {
@@ -23,8 +29,15 @@ export default function Friends() {
     return <div>Error: please login in</div>;
   }
 
-  const deleteFri = (friend) => {
+  const deleteFri = async (friend, e) => {
     console.log(friend);
+    if (friend._id) {
+      let deleteF = await users.deleteFriend(friend._id);
+      console.log(deleteF);
+      if (deleteF.code === 200) {
+        navigate("/login");
+      }
+    }
   };
 
   return (
@@ -34,7 +47,15 @@ export default function Friends() {
           {friendsData.map((friend) => (
             <ListGroup.Item action href="">
               {friend.username}
-              <Button onClick={deleteFri(friend)}>Delete</Button>
+
+              <Button
+                onClick={(e) => {
+                  console.log(e);
+                  deleteFri(friend, e);
+                }}
+              >
+                Delete
+              </Button>
             </ListGroup.Item>
           ))}
         </ListGroup>
